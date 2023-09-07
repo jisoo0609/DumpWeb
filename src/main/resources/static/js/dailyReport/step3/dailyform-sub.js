d4df6954391340fc3530591e102fd71bfba82520
JiJuOh <godinggool@gmail.com>
2023-09-07 11:58:49 +0900

    feat, remove: 결제버튼을 통해서만 결재일 이 나타남. 안쓰는 css 삭제

--- src/main/resources/static/js/dailyReport/step3/dailyform-sub.js
+++ src/main/resources/static/js/dailyReport/step3/dailyform-sub.js
@@ -1,8 +1,33 @@
+/* function :   */
+const popup = document.getElementById('popup')
+
 function openPop() {
-    document.getElementById('popup').style.display = 'flex';
+    popup.style.display = 'flex';
 }
 
 function closePop() {
-    document.getElementById('popup').style.display = 'none';
+    popup.style.display = 'none';
+}
+
+
+/* function : 결재버튼을 통해서만 체크박스를 체크하거나 해제할 수 있다.  */
+const dateDisplay = document.getElementById('dateDisplay');
+const currentDate = document.getElementById('currentDate');
+currentDate.textContent = '연도- 월- 일';
+
+function checkBox() {
+     const checkbox = document.getElementById('checkbox');
+     checkbox.checked = !checkbox.checked;
+
+     if (checkbox.checked) {
+         const today = new Date();
+         const year = today.getFullYear();
+         const month = String(today.getMonth() + 1).padStart(2, '0');
+         const day = String(today.getDate()).padStart(2, '0');
+         const dateString = `${year}-${month}-${day}`;
+         currentDate.textContent = year+'-'+month+'-'+day;
+     } else {
+         currentDate.textContent = '연도- 월- 일';
+     }
 }
 
