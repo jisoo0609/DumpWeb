cc3fe1ae846685e402174bca73b8b5987b3e5531
jisubaek <bbbjisu@gmail.com>
2023-09-11 16:09:40 +0900

    차계부 insert 로직 작성완료

--- src/main/java/com/dispatch/dump/dailyReportModule/controller/Step5Controller.java
+++ src/main/java/com/dispatch/dump/dailyReportModule/controller/Step5Controller.java
@@ -1,12 +1,16 @@
 package com.dispatch.dump.dailyReportModule.controller;
 
 import com.dispatch.dump.commonModule.db.dto.DailyReport;
+import com.dispatch.dump.commonModule.db.dto.DailyReportStep5;
 import com.dispatch.dump.dailyReportModule.service.DailyReportService;
+import com.dispatch.dump.dailyReportModule.service.Step5Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/dailyReport")
@@ -14,8 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class Step5Controller {
 
     private final DailyReportService dailyReportService;
+    private final Step5Service step5Service;
 
     @RequestMapping(value = "/carcareform", method = RequestMethod.GET)
+    @ResponseBody
     public String step5main(Model model, DailyReport dailyReport) {
          dailyReportService.list(model, dailyReport);
         return "/dailyReport/step5/carcareform";
@@ -27,4 +33,9 @@ public class Step5Controller {
         return "/dailyReport/step5/nextcarcare";
     }
 
+    @RequestMapping(value = "/carcare/ajax/save", method = RequestMethod.POST)
+    @ResponseBody
+    public String save(@RequestBody DailyReportStep5 dailyReportStep5) {
+        return step5Service.save(dailyReportStep5);
+    }
 }
