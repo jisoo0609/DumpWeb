3bf30b74f5ee92950c93c26dff366a38e38c87af
scyllacore <yms8795@gmail.com>
2023-09-12 11:28:43 +0900

    chore : 프로젝트 버전 다운그레이드.

--- src/main/java/com/dispatch/dump/loginModule/service/LoginService.java
+++ src/main/java/com/dispatch/dump/loginModule/service/LoginService.java
@@ -9,8 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Service
