1b5834082121ecf4a06a44db001a55bf763e042d
scyllacore <yms8795@gmail.com>
2023-09-12 10:16:00 +0900

    chore : 메뉴 항목 순서 변경에 따라 파일 이동.

--- src/main/java/com/dispatch/dump/dailyReportModule/controller/Step8Controller.java
+++ src/main/java/com/dispatch/dump/dailyReportModule/controller/Step8Controller.java
@@ -1,9 +1,12 @@
 package com.dispatch.dump.dailyReportModule.controller;
 
+import com.dispatch.dump.commonModule.db.dto.DailyReport;
 import com.dispatch.dump.dailyReportModule.service.DailyReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/dailyReport")
@@ -11,5 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Step8Controller {
 
     private final DailyReportService dailyReportService;
+
+    @RequestMapping(value = "/receipts", method = RequestMethod.GET)
+    public String step7(Model model, DailyReport dailyReport) {
+        dailyReportService.list(model, dailyReport);
+        return "/dailyReport/step8/receipts";
+
+    }
 
 }
