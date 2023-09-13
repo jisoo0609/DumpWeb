1bff9e17e236c350ced1d7c6e1be0a66c16bbc17
CWJ9703\cwj97 <cwj9703@gmail.com>
2023-09-13 16:27:53 +0900

    feat : 차량관리 차량조회 날짜 완료

--- src/main/java/com/dispatch/dump/dailyReportModule/controller/Step6Controller.java
+++ src/main/java/com/dispatch/dump/dailyReportModule/controller/Step6Controller.java
@@ -1,18 +1,15 @@
 package com.dispatch.dump.dailyReportModule.controller;
 
 import com.dispatch.dump.commonModule.db.dto.DailyReport;
-import com.dispatch.dump.commonModule.db.dto.DailyReportStep2Sub;
 import com.dispatch.dump.commonModule.db.dto.DailyReportStep6;
 import com.dispatch.dump.dailyReportModule.service.DailyReportService;
-import com.dispatch.dump.dailyReportModule.service.Step2Service;
 import com.dispatch.dump.dailyReportModule.service.Step6Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -24,16 +21,42 @@ public class Step6Controller {
     private final Step6Service step6Service;
 
     @RequestMapping(value = "/carcarelist", method = RequestMethod.GET)
-    public String step6(Model model, DailyReport dailyReport) {return "/dailyReport/step6/carcarelist";}
+    public String step6(Model model, DailyReport dailyReport) {
+        return "/dailyReport/step6/carcarelist";
+    }
 
     @RequestMapping(value = "/step6/getcarlist", method = RequestMethod.GET)
     @ModelAttribute("carlist")
     public List<DailyReportStep6> getcarlist() {
         List<DailyReportStep6> carlist = step6Service.getcarlist();
-
         return carlist;
     }
 
-
-
+    @RequestMapping(value = "/step6/findDailyReportClub", method = RequestMethod.GET)
+    @ModelAttribute("carlist")
+    public List<DailyReportStep6> findDailyReportClub(
+            String carNo,
+            String drvClub
+    ) {
+        List<DailyReportStep6> carlist = step6Service.findDailyReportClub(carNo, drvClub);
+        return carlist;
+    }
+    @RequestMapping(value = "/step6/search", method = RequestMethod.POST)
+    @ModelAttribute("carlist")
+    public List<DailyReportStep6> searchDailyReport(
+            @RequestParam("carNo") String carNo,
+            @RequestParam("searchType") String searchType,
+            @RequestParam(value = "drvClub", required = false) String drvClub
+    ) {
+        if ("date".equals(searchType)) {
+            // 날짜 기준으로 검색
+            return step6Service.getcarlist();
+        } else if ("item".equals(searchType)) {
+            // 품목 기준으로 검색
+            return step6Service.findDailyReportClub(carNo, drvClub);
+        } else {
+            // 다른 경우에 대한 처리 (예: 오류 처리)
+            return Collections.emptyList();
+        }
+    }
 }
