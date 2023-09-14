bbb3277b278ab5c10727319fc02db2785ee48509
scyllacore <yms8795@gmail.com>
2023-09-14 19:53:58 +0900

    feaf, refactor : step6 전체 시스템 구현.

--- src/main/java/com/dispatch/dump/dailyReportModule/controller/Step6Controller.java
+++ src/main/java/com/dispatch/dump/dailyReportModule/controller/Step6Controller.java
@@ -22,25 +22,14 @@ public class Step6Controller {
     private final Step6Service step6Service;
 
     @RequestMapping(value = "/carcarelist", method = RequestMethod.GET)
-    public String step6(Model model, DailyReport dailyReport) {
-        model.addAttribute("defaultList", step6Service.getCarListByDate());
+    public String step6(Model model) {
+        model.addAttribute("defaultList", step6Service.getDefaultCarList());
         return "/dailyReport/step6/carcarelist";
     }
 
     @RequestMapping(value = "/carcarelist", method = RequestMethod.POST)
     @ResponseBody
     public List<DailyReportStep6> searchDailyReport(DailyReportStep6SelectForm selectForm) {
-        // 날짜 기준으로 검색
-        if (selectForm.getSearchType().equals("date")) {
-            return step6Service.getCarListByDate();
-        }
-        // 품목 기준으로 검색
-        else if (selectForm.getSearchType().equals("item")) {
-            return step6Service.getCarListByItem(selectForm.getCarNo(), selectForm.getSelectOption());
-        }
-        // 다른 경우에 대한 처리 (예: 오류 처리)
-        else {
-            return Collections.emptyList();
-        }
+        return step6Service.getCarListByOption(selectForm);
     }
 }
