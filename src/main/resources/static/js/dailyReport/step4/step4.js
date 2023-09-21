ef6843efe40142a45b965acce7f24f72ae78f01f
hansol <fpdlqm21@naver.com>
2023-09-21 16:39:09 +0900

    상세검색 조건에 따른 기능 수행 함수 작성

--- src/main/resources/static/js/dailyReport/step4/step4.js
+++ src/main/resources/static/js/dailyReport/step4/step4.js
@@ -4,7 +4,32 @@ function getList(){
         type: "POST",
         data: $("[name=data_frm]").serialize(),
         success: function(data){
-           //서버에서 받은 데이터 처리할 함수 입력
+           printTable(data)//서버에서 받은 데이터 처리할 함수 입력
         }
     });
+}
+
+function printTable(datas){
+    const tableBody = document.querySelector(".list-table tbody");
+    tableBody.innerHTML = "";
+
+    datas.forEach((data, idx) => {
+        const row = document.createElement("tr");
+        let order = [
+            data.carSubmit, data.date,
+            data.item, data.fromsite, data.tosite, data.Qty, data.Qtyup.toLocaleString()
+        ];
+
+        row.innerHTML = `
+                    <td>${order[0]}</td>
+                    <td>${order[1]}</td>
+                    <td>${order[2]}</td>
+                    <td>${order[3]}</td>
+                    <td>${order[4]}</td>
+                    <td>${order[5]}</td>
+                    <td>${order[6]}</td>
+        `;
+
+        tableBody.appendChild(row);
+    })
 }
