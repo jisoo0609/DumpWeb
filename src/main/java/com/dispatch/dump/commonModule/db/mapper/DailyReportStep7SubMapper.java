48329beefcd7798facbd2165ee81a1917850f05b
kse816 <kse816@naver.com>
2023-09-26 09:36:57 +0900

    DTO 수정 및 등록 controller service mappper 수정

--- src/main/java/com/dispatch/dump/commonModule/db/mapper/DailyReportStep7SubMapper.java
+++ src/main/java/com/dispatch/dump/commonModule/db/mapper/DailyReportStep7SubMapper.java
@@ -1,10 +1,12 @@
 package com.dispatch.dump.commonModule.db.mapper;
 
+import com.dispatch.dump.commonModule.db.dto.DailyReportStep3Sub;
 import com.dispatch.dump.commonModule.db.dto.DailyReportStep7Main;
 import com.dispatch.dump.commonModule.db.dto.DailyReportStep7Sub;
 
 public interface DailyReportStep7SubMapper {
 
-
+    /* FUNCTION ::  등록 */
+    int insertDailyReportSub(DailyReportStep7Sub DailyReportStep7Sub);
 
 }
--- src/main/java/com/dispatch/dump/dailyReportModule/service/Step7Service.java
+++ src/main/java/com/dispatch/dump/dailyReportModule/service/Step7Service.java
@@ -3,6 +3,10 @@ package com.dispatch.dump.dailyReportModule.service;
 import com.dispatch.dump.commonModule.db.dto.DailyReportStep7CarNo;
 import com.dispatch.dump.commonModule.db.dto.Login;
 import com.dispatch.dump.commonModule.db.mapper.DailyReportStep7Mapper;
+import com.dispatch.dump.commonModule.db.mapper.DailyReportStep7MainMapper;
+import com.dispatch.dump.commonModule.db.mapper.DailyReportStep7SubMapper;
+import com.dispatch.dump.commonModule.db.dto.DailyReportStep7Main;
+import com.dispatch.dump.commonModule.db.dto.DailyReportStep7Sub;
 import com.dispatch.dump.commonModule.util.CommonUtil;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -18,8 +22,49 @@ import java.util.Map;
 public class Step7Service {
 
     private final CommonUtil commonUtil;
+    private final DailyReportStep7MainMapper dailyReportStep7MainMapper;
+    private final DailyReportStep7SubMapper dailyReportStep7SubMapper;
     private final DailyReportStep7Mapper step7Mapper;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    //제출처 주문 정보 저장
+    public String saveOrder(DailyReportStep7Main dailyReportStep7Main, DailyReportStep7Sub dailyReportStep7Sub){
+
+        Map<String, Object> rtnMap = commonUtil.returnMap();
+        HttpSession session = commonUtil.getSession();
+
+        try {
+            Login loginData = (Login) session.getAttribute("loginInfo");
+            dailyReportStep7Main.setCarNo(loginData.getUserId());
+            dailyReportStep7Main.setSheetSS2(Integer.parseInt(loginData.getUuserID()));
+
+            //제출처, date 확인
+            DailyReportStep7Main result=dailyReportStep7MainMapper.findDailyReportMainList(dailyReportStep7Main);
+            System.out.println(result);
+            if(result != null){
+                //제출처 정보가 있을때
+                dailyReportStep7Sub.setSheetID2(result.getSheetID());
+                dailyReportStep7Sub.setSheetsubSS2(Integer.parseInt(loginData.getUuserID()));
+
+                dailyReportStep7SubMapper.insertDailyReportSub(dailyReportStep7Sub);
+                rtnMap.put("httpCode", 200);
+            }else{
+
+                dailyReportStep7Main.setSheetSS2(Integer.parseInt(loginData.getUuserID()));
+                dailyReportStep7MainMapper.insertDailyReportMain(dailyReportStep7Main);
+
+                dailyReportStep7Sub.setSheetID2(dailyReportStep7Main.getSheetID());
+                dailyReportStep7Sub.setSheetsubSS2(Integer.parseInt(loginData.getUuserID()));
+
+                dailyReportStep7SubMapper.insertDailyReportSub(dailyReportStep7Sub);
+                rtnMap.put("httpCode", 200);
+            }
+        } catch (Exception e) {
+            log.error("Exception["+ e.getMessage() +"]");
+        }
+        return commonUtil.jsonFormatTransfer(rtnMap);
+
+    }
 
     public String saveCarData(DailyReportStep7CarNo dailyReportStep7CarNo) {
         Map<String, Object> rtnMap = commonUtil.returnMap();
