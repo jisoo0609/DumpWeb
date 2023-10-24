829b920b705b668b6e34d922d2ec9380811275c9
baektom <baektom77@gmail.com>
2023-10-24 11:22:07 +0900

    step7

--- src/main/resources/static/js/dailyReport/step7/order.js
+++ src/main/resources/static/js/dailyReport/step7/order.js
@@ -125,71 +125,9 @@ function addTableRow(data) {
 
       html += '</tr>';
     }
-    console.log(html);
+    console.log(html + '\n');
     // html += '</table>';
   }
     // 데이터를 표시할 위치에 추가
     $('#tBody').html(html);
-
-    row.setAttribute("receipt-subID", data.sheetsubID);
-    row.setAttribute("receipt-sheetID", data.sheetID);
-    row.setAttribute("receipt-writerIDX", data.writerIDX);
-    row.setAttribute("receipt-sheetSS2", data.sheetSS2);
-    tableBody.appendChild(row);
-}
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const queryString = window.location.search;
-    const params = new URLSearchParams(queryString);
-
-    let sheetsubID = params.get("subID");
-    let sheetID = params.get("sheetID")
-    console.log("sheetsubId :" + sheetsubID);
-
-    if (sheetsubID !== null) {
-        getSheetsubIDDataByParams(sheetsubID, sheetID);
-    }
-    // clickListThAndRedirect();
-});
-
-
-function getSheetsubIDDataByParams(sheetsubID, sheetID) {
-    $.ajax({
-        url: "/dailyReport/ajax/details",
-        type: "POST",
-        data: {sheetsubID: sheetsubID , sheetID: sheetID},
-        success: function (data) {
-            document.getElementById('date').value=data.date;
-            document.getElementById('fromsite').value=data.fromsite;
-            document.getElementById('tosite').value=data.tosite;
-            document.getElementById('item').value=data.item;
-            document.getElementById('Qty').value=data.qty;
-            document.getElementById('carNo').value=data.carNo;
-            document.getElementById('Qtyup').value=data.qtyup;
-        }
-    })
-}
-
-/* 리스트의 행 클릭시, 상태값에 따라 파라미터와 함께 step3 or step7로 이동하도록 처리. */
-function clickListThAndRedirect(){
-    const listRow = document.querySelector("table tbody");
-
-    listRow.addEventListener("click", (event) => {
-        const currStatus = event.target.parentElement.querySelector(".currStatus"); // 상태 열
-
-        if (currStatus) {
-            const status = currStatus.textContent;
-            console.log("Clicked Status:", status);
-            let subID = event.target.parentElement.getAttribute("receipt-subID")
-            let sheetID = event.target.parentElement.getAttribute("receipt-sheetID")
-            let writerIDX = event.target.parentElement.getAttribute("receipt-writerIDX") //작성자구별은 추후 구현
-            let sheetSS2 = event.target.parentElement.getAttribute("receipt-sheetSS2")
-            console.log("writerIDX?" + writerIDX)
-            console.log("sheetSS2?" + sheetSS2)
-
-
-            window.location.href = "/dailyReport/orderform" + "?subID=" +  subID + "&sheetID=" + sheetID;
-        }
-    });
 }
